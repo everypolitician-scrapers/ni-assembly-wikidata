84814d722e5839c25b4abe766b963ae53a16cb97
Tony Bowden <tony@tmtm.com>
2015-10-17 12:09:16 +0100

    Fetch by individual terms
    The 'Members of Assembly' claim includes people from prior 1970s/1980s
    Assemblies too. Instead fetch based on membership of the individual
    tersm.

--- scraper.rb
+++ scraper.rb
@@ -11,12 +11,15 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
 def ids_from_claim(claim_str)
-  url = 'https://wdq.wmflabs.org/api?q=claim[39:3272410]'
+  url = 'https://wdq.wmflabs.org/api?q=%s' % claim_str
   json = JSON.parse(open(url).read, symbolize_names: true)
   json[:items].map { |id| "Q#{id}" }
 end
 
-ids_from_claim('39:3272410').each do |id|
+terms = %w(Q21128140 Q21128144 Q21128152 Q21124329)
+claim = terms.map { |t| "claim[463:%d]" % t.sub('Q','').to_i }.join('+OR+')
+
+ids_from_claim(claim).each do |id|
   data = WikiData::Fetcher.new(id: id).data or next
   ScraperWiki.save_sqlite([:id], data)
 end
