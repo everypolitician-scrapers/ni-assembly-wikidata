6688e5c1e21af0b3c312e4ad431da85a0a6eee32
Tony Bowden <tony@tmtm.com>
2016-02-22 17:09:30 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'pry'
 require 'wikidata/fetcher'
 
 terms = %w(Q21128140 Q21128144 Q21128152 Q21124329)
@@ -9,7 +8,7 @@ claim = terms.map { |t| "claim[463:%d]" % t.sub('Q','').to_i }.join(' OR ')
 
 ids = EveryPolitician::Wikidata.wdq(claim)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: true)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
 
 
