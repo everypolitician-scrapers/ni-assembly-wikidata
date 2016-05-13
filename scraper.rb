33d1eba1806e99ec61d70782b13b9fc48108076b
Tony Bowden <tony@tmtm.com>
2016-05-13 06:09:14 +0100

    Add 5th Assembly

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,9 @@
 
 require 'wikidata/fetcher'
 
-terms = %w(Q21128140 Q21128144 Q21128152 Q21124329)
+terms = %w(Q21128140 Q21128144 Q21128152 Q21124329 Q24050037)
 claim = terms.map { |t| "claim[463:%d]" % t.sub('Q','').to_i }.join(' OR ')
-
 ids = EveryPolitician::Wikidata.wdq(claim)
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
-
 
