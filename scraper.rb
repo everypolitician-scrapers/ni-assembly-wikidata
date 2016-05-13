68c13699d67b0fa4dbc034f6f20d7b09c7de09da
Tony Bowden <tony@tmtm.com>
2016-05-13 08:19:00 +0100

    Also fetch by Category

--- scraper.rb
+++ scraper.rb
@@ -7,5 +7,7 @@ terms = %w(Q21128140 Q21128144 Q21128152 Q21124329 Q24050037)
 claim = terms.map { |t| "claim[463:%d]" % t.sub('Q','').to_i }.join(' OR ')
 ids = EveryPolitician::Wikidata.wdq(claim)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
+names = WikiData::Category.new('Category:Northern Ireland MLAs 2016–', 'en').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names }, output: false)
 
