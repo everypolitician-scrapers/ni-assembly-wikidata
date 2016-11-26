0bec622a98eec0444bc3a1055eec5f5d42dec9a2
Tony Bowden <tony@tmtm.com>
2016-11-26 10:35:46 +0000

    Replace WDQ with SPARQL

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,11 @@
 
 require 'wikidata/fetcher'
 
+sparq = 'SELECT ?item WHERE { ?item wdt:P463 wd:%s . }'
 terms = %w(Q21128140 Q21128144 Q21128152 Q21124329 Q24050037)
-claim = terms.map { |t| "claim[463:%d]" % t.sub('Q','').to_i }.join(' OR ')
-ids = EveryPolitician::Wikidata.wdq(claim)
+ids = terms.map { |id| EveryPolitician::Wikidata.sparql(sparq % id) }.reduce(:|)
 
 names = WikiData::Category.new('Category:Northern Ireland MLAs 2016–', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
 
