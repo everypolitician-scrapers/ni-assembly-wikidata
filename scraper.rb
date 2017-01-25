9bbb8a9ee6009a47657f3dfe96481113d0bf1a63
Tony Bowden <tony@tmtm.com>
2017-01-25 13:49:59 +0000

    Update Category name

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,7 @@ sparq = 'SELECT ?item WHERE { ?item wdt:P463 wd:%s . }'
 terms = %w(Q21128140 Q21128144 Q21128152 Q21124329 Q24050037)
 ids = terms.map { |id| EveryPolitician::Wikidata.sparql(sparq % id) }.reduce(:|)
 
-names = WikiData::Category.new('Category:Northern Ireland MLAs 2016–', 'en').member_titles
+names = WikiData::Category.new('Category:Northern Ireland MLAs 2016–2017', 'en').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
 
