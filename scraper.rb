44f8bca75d22254229fdf270ede0db23a3a0d95d
Tony Bowden <tony@tmtm.com>
2017-03-06 12:47:40 +0000

    Only fetch by P39 memberships
    All members should now have this.

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,8 @@
 
 require 'wikidata/fetcher'
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P463 wd:%s . }'
-terms = %w(Q21128140 Q21128144 Q21128152 Q21124329 Q24050037)
-ids = terms.map { |id| EveryPolitician::Wikidata.sparql(sparq % id) }.reduce(:|)
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q3272410 . }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
 
-names = WikiData::Category.new('Category:Northern Ireland MLAs 2016–2017', 'en').member_titles
-
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
 
