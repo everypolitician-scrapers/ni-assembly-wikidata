b87b6270f8a332ee47de0dbebe571b7a923acfd8
Tony Bowden <tony@tmtm.com>
2018-10-04 12:42:01 +0100

    Fetch all subclasses of Q3272410

--- scraper.rb
+++ scraper.rb
@@ -4,9 +4,8 @@
 require 'everypolitician'
 require 'wikidata/fetcher'
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q3272410 . }'
+sparq = 'SELECT ?item WHERE { ?item wdt:P39/wdt:P279* wd:Q3272410 }'
 members = EveryPolitician::Wikidata.sparql(sparq)
 existing = EveryPolitician::Index.new.country("Northern-Ireland").lower_house.popolo.persons.map(&:wikidata).compact
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: existing | members)
-
